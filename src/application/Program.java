8bef594823c57d94bcee7d3fb1cd9854d831aae5
Rafael De Luca <deluca1712@gmail.com>
2022-11-06 11:22:46 -0300

    Refatorando a classe Conta

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 import entities.Conta;
 
@@ -25,17 +26,32 @@ public class Program {
 		c2.saque(599.00);
 		
 		
+		Scanner input = new Scanner(System.in);
+		Conta c3 = new Conta("poupança25109", null);
+		System.out.print("\nConfirme o seu nome como correntistas: ");
+		String nome = input.nextLine();		
+		c3.setTitular(nome);
+		System.out.print("Deseja fazer um depósito inicial: (s/n)");
+		char resposta = input.next().charAt(0);
+		if (resposta =='s' || resposta =='S') {
+			System.out.print("Informe o valor do Deposito: ");
+			double depositoInicial = input.nextDouble();
+			c3.deposito(depositoInicial);
+		}
+		
+		
 		System.out.println("\nImprimindo valores após saques e depósitos");
 		
 		List<Conta> lista = new ArrayList<Conta>();
 		lista.add(c1);
 		lista.add(c2);
+		lista.add(c3);
 		
 		for (Conta apelido: lista) {
 			System.out.println(apelido);
 		}
 		
-		
+		input.close();
 				
 		
 	}
--- src/entities/Conta.java
+++ src/entities/Conta.java
@@ -5,7 +5,7 @@ public class Conta {
 	private String numeroConta;
 	private String titular;
 	private double saldo;
-	private double depositoInicial=0; //inicia com zero se não houver deposito
+	
 	
 	/* não fazer o construtor vazio, obrigado a setar um numero da conta e nome ao instanciar uma conta
 	public Conta () {
@@ -18,15 +18,14 @@ public class Conta {
 		this.numeroConta = numeroConta;
 		this.titular = titular;
 		this.saldo = 0.0;
-		depositoInicial = 0.0;
+		
 	}
 
 	public Conta(String numeroConta, String titular, double depositoInicial) {
 		super();
 		this.numeroConta = numeroConta;
 		this.titular = titular;		
-		this.depositoInicial = depositoInicial;
-		this.saldo = depositoInicial;
+		this.deposito(depositoInicial); //acrescentar o valor do deposito na conta
 	}
 
 	public String getNumeroConta() {
@@ -50,27 +49,20 @@ public class Conta {
 	public double getSaldo() {
 		return saldo;
 	}
-
-	/* não pode setar o saldo da conta, só movimentar por deposito ou por saque
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-	*/
 	
-	public double getDepositoInicial() {
-		return depositoInicial;
-	}
 	
 	public void deposito(double valor) {
 		this.saldo += valor;
-		System.out.println("\nDepósito do valor " + valor + " realizado com sucesso na conta do titular " + this.getTitular());
+		System.out.println("\nDepósito do valor " + valor + " realizado com sucesso na conta do titular " + this.getTitular() + ".\n"
+				+ "Seu saldo agora é " + String.format("R$ %.2f", this.saldo));
 	}
 	
 	public void saque(double valor) {
 		double taxaSaque = 5.0;
 		if (this.saldo > (taxaSaque + valor)) {
 			this.saldo = this.saldo - taxaSaque -  valor;
-			System.out.println("\nSaque do valor " + valor + " realizado com sucesso na conta do titular " + this.getTitular());
+			System.out.println("\nSaque do valor " + valor + " realizado com sucesso na conta do titular " + this.getTitular() + ".\n"
+					+ "Seu saldo agora é " + String.format("R$ %.2f", this.saldo));
 		} else {
 			System.out.println("\nVocê não tem saldo para realizar essa operação de sacar " + valor 
 					+ "\nA taxa de saque é " + taxaSaque + " e o seu saldo na conta é " + this.getSaldo() );
@@ -83,7 +75,7 @@ public class Conta {
 		return "Conta [numeroConta =" + numeroConta 
 				+ ", titular =" + titular 
 				+ ", saldo =" + String.format("R$ %.2f", saldo) 
-				+ ", depositoInicial =" + String.format("R$ %.2f", depositoInicial) + "]";
+				+  "]";
 	}
 
 	/* Não pode setar um novo deposito incial, ou depositou na hora de criar a conta ou não depositou
@@ -91,11 +83,5 @@ public class Conta {
 		this.depositoInicial = depositoInicial;
 	}
 	*/	
-	
-	
-	
-	
-	
-	
 
 }
